f9602605a0cb4a2e29958eb0ddce24215c0e2869
Intern-Yago <yagovictorbotafogo@gmail.com>
2023-01-25 23:25:18 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import JoditEditor from "jodit-react";
+import { useRef, useState } from "react";
+import "./App.css" 
 
 function App() {
+  const editor = useRef(null)
+  const [content, setContent] = useState('')
+
+  const config = {
+    reloadonly: false,
+    enableDragAndDropFileToEditor: true,
+    editorClassName: false,
+    uploader: {
+      "insertImageAsBase64URI": true
+    },
+    theme: 'default',
+    toolbarButtonSize: 'middle',
+    hidePoweredByJodit: true,
+    direction: 'ltr',
+    image:{
+      editAlign: true,
+      editAlt:true,
+      editBorderRadius: true,
+      editLink: true,
+      editSize: true,
+      editTitle: true,
+      openOnDblClick: true,
+      showPreview: true,
+    }
+  }
+
+  const handleUpdate = (event)=>{
+    const editorContent = event.target.value
+    setContent(editorContent)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <JoditEditor
+
+        ref={editor}
+        value={content}
+        config={config}
+        onBlur={handleUpdate}
+        onChange={(newContent)=>{
+
+        }}
+      />
+
     </div>
   );
 }
